be4ea6937a521a3a36e444531619b2032b86413a
Loreto Delgado III <ldelgadothethird@gmail.com>
2021-03-08 14:26:01 -0600

    added error function if an error occurs. added request for upgraded. added request for onsuccess to assign result to db.

--- public/db.js
+++ public/db.js
@@ -1,3 +1,31 @@
 let db;
 
 const request = indexedDB.open("budget", 1);
+
+request.onupgradeneeded = function (event) {
+    db = event.target.result;
+    const pendingStore = db.createObjectStore("pending", { autoIncrement: true });
+};
+
+request.onsuccess =  function (event) {
+    db = event.target.result;
+
+    if (navigator.onLine) {
+        checkDatabase();
+    }
+}
+
+request.onerror = function(event) {
+    console.log("Error: " + event.target.errorCode);
+  };
+
+function saveRecord(record) {
+
+}
+
+function checkDatabase() {
+    
+}
+
+// listen for app coming back online
+window.addEventListener('online', checkDatabase);

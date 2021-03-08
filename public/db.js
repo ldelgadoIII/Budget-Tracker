cee8e492cf324d9c8b8ba115046de639d007e93f
Loreto Delgado III <ldelgadothethird@gmail.com>
2021-03-08 14:39:43 -0600

    added checkDatabase function for any saved records in db

--- public/db.js
+++ public/db.js
@@ -26,6 +26,31 @@ function saveRecord(record) {
 }
 
 function checkDatabase() {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const pendingStore = transaction.objectStore("pending");
+    const getAll = pendingStore.getAll();
+
+    getAll.onsuccess = function () {
+      if (getAll.result.length > 0) {
+        fetch("/api/transaction/bulk", {
+          method: 'POST',
+          body: JSON.stringify(getAll.result),
+          headers: {
+          Accept: 'application/json, text/plain, */*',
+					'Content-Type': 'application/json',
+          }
+        })
+				.then((response) => response.json())
+        .then(() => {
+          // open a transaction on your pending db
+          const transaction = db.transaction(["pending"], "readwrite");
+          // access your pending object store
+          const pendingStore = transaction.objectStore("pending")
+          // clear all items in your store
+          pendingStore.clear();
+        });
+      }
+    }
 }
 
 // listen for app coming back online

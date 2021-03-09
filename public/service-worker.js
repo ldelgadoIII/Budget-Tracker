c23578cd562aa11e32c9f60d891b6c55a18fd48f
Loreto Delgado III <ldelgadothethird@gmail.com>
2021-03-09 14:56:40 -0600

    added feature that adds data into data cache at service worker install

--- public/service-worker.js
+++ public/service-worker.js
@@ -17,6 +17,10 @@ const FILES_TO_CACHE = [
 // install
 self.addEventListener("install", function(evt) {
     evt.waitUntil(
+        caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/transaction"))
+        );
+
+    evt.waitUntil(
       caches.open(CACHE_NAME).then(cache => {
         console.log("Your files were pre-cached successfully!");
         return cache.addAll(FILES_TO_CACHE);

ff46ea0957f9ca8a5a1cc7ae541e0a438809bc13
Loreto Delgado III <ldelgadothethird@gmail.com>
2021-03-09 14:02:48 -0600

    added cache variables to service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,14 @@
 console.log("Service-worker file connected!")
+
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1;";
+
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.webmanifest',
+  '/index.js',
+  '/db.js',
+  "assets/images/icons/icon-72x72.png",
+  "assets/images/icons/icon-96x96.png",
+];
